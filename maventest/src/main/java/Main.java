797677189952cdcedd379980dc0c31cbf94ffe0e
Majoros Aniko <aniko.majoros87@gmail.com>
2019-04-03 11:56:30 +0200

    refactor(imdbtest) - change folder structure

---
+++ maventest/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.google.com/");
+    }
+}
