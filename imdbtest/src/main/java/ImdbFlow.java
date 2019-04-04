918c9fccf0e0b352983782cbcb258de637b72e1d
Majoros Aniko <aniko.majoros87@gmail.com>
2019-04-04 14:46:18 +0200

    refactor(search): choose first suggestion

--- imdbtest/src/main/java/ImdbFlow.java
+++ imdbtest/src/main/java/ImdbFlow.java
@@ -1,30 +1,42 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ImdbFlow {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         WebDriver driver = new FirefoxDriver();
+        Actions builder = new Actions(driver);
 
+        //get main page
         driver.get("https://www.imdb.com/");
         driver.manage().window().maximize();
 
+        //sign in
         driver.findElement(By.id("imdb-signin-link")).click();
-
         driver.findElement(By.linkText("Sign in with IMDb")).click();
-
         driver.findElement(By.id("ap_email")).sendKeys("aniko.majoros87@gmail.com");
         driver.findElement(By.id("ap_password")).sendKeys("imdbpassword");
         driver.findElement(By.id("signInSubmit")).click();
 
+        //search
+        WebDriverWait wait = new WebDriverWait(driver, 5);
         driver.findElement(By.id("navbar-query")).sendKeys("shawshank");
-        driver.findElement(By.id("navbar-submit-button")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"navbar-suggestionsearch\"]/div[1]/a")));
+        driver.findElement(By.xpath("//*[@id=\"navbar-suggestionsearch\"]/div[1]/a")).click();
 
-        driver.findElement(By.linkText("Watchlist")).click();
+        //navigate to watchlist
+        /*driver.findElement(By.linkText("Watchlist")).click();
         driver.findElement(By.xpath("//*[@id=\"center-1-react\"]/div/div[1]/div/div[1]/a))")).click();
 
-        driver.findElement(By.id("add-to-list-search")).sendKeys("shawshank");
+        driver.findElement(By.id("add-to-list-search")).sendKeys("shawshank");*/
+        Thread.sleep(3000);
+        driver.close();
     }
 }
